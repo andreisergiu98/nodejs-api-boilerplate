2b3f12456cd68f6329db71b8927249ad1b70752b
Andrei Pampu <pampu.s.andrei@gmail.com>
2020-11-24 22:06:20 +0200

    Create a decorator helper for postgres errors

--- src/lib/resource-service/resource-service.ts
+++ src/lib/resource-service/resource-service.ts
@@ -2,7 +2,8 @@ import {EntityTarget, FindManyOptions, FindOneOptions} from 'typeorm';
 import {QueryDeepPartialEntity} from 'typeorm/query-builder/QueryPartialEntity';
 
 import {DbClient} from '../db-client';
-import {handleQueryError} from '../query-error';
+import {AppError} from '../app-error';
+import {HandleDbQueryError} from './resource-service-utils';
 
 export class ResourceService<T> {
     constructor(
@@ -11,68 +12,90 @@ export class ResourceService<T> {
     ) {
     }
 
-    getById = async (id: number, options: FindOneOptions<T>) => {
-        return this.db.manager.findOne(this.targetEntity, id, options);
-    };
+    async getById(id: number, options?: FindOneOptions<T>) {
+        const res = await this.db.manager.findOne(this.targetEntity, id, options);
+        if (res) return res;
 
-    getMany = async (options: FindManyOptions<T>) => {
+        throw  new AppError(404, 'Entity not found', this.namespace);
+    }
+
+    async getMany(options?: FindManyOptions<T>) {
         return this.db.manager.find(this.targetEntity, options);
-    };
+    }
 
-    create = async (payload: T) => {
-        try {
-            return this.db.manager
-                .createQueryBuilder()
-                .insert()
-                .into(this.targetEntity)
-                .values(payload)
-                .returning('*')
-                .execute();
-        } catch (e) {
-            handleQueryError(this.db, this.targetEntity, e);
-        }
-    };
+    @HandleDbQueryError()
+    async create(payload: T | T[]) {
+        const isArray = Array.isArray(payload);
 
-    update = async (id: number, payload: T) => {
-        try {
-            return this.patch(id, payload);
-        } catch (e) {
-            handleQueryError(this.db, this.targetEntity, e);
-        }
-    };
+        const res = await this.db.manager
+            .createQueryBuilder()
+            .insert()
+            .into(this.targetEntity)
+            .values(payload)
+            .returning('*')
+            .execute();
 
-    updateMany = async (payload: T[]) => {
-        try {
-            await this.db.manager.save(this.targetEntity, payload);
-        } catch (e) {
-            handleQueryError(this.db, this.targetEntity, e);
-        }
-    };
+        return (isArray ? res.raw : res.raw[0]);
+    }
 
-    patch = async (id: number, payload: QueryDeepPartialEntity<T>) => {
-        try {
-            return this.db.manager
-                .createQueryBuilder()
-                .update(this.targetEntity)
-                .where('id = :id', {id})
-                .set(payload)
-                .returning('*')
-                .execute();
-        } catch (e) {
-            handleQueryError(this.db, this.targetEntity, e);
-        }
-    };
+    async update(id: number, payload: T) {
+        return this.patch(id, payload);
+    }
 
-    delete = async (id: number) => {
-        try {
-            await this.db.manager
-                .createQueryBuilder()
-                .delete()
-                .from(this.targetEntity)
-                .where('id = :id', {id})
-                .execute();
-        } catch (e) {
-            handleQueryError(this.db, this.targetEntity, e);
+    @HandleDbQueryError()
+    async updateMany(payload: T[]) {
+        return this.db.manager.save(this.targetEntity, payload, {
+            chunk: 10000,
+        });
+    }
+
+    @HandleDbQueryError()
+    async patch(id: number, payload: QueryDeepPartialEntity<T>) {
+        const exists = await this.existsById(id);
+        if (!exists) {
+            throw new AppError(404, `Entity doesn't exist`, this.namespace);
         }
-    };
+
+        return this.db.manager
+            .createQueryBuilder()
+            .update(this.targetEntity)
+            .where('id = :id', {id})
+            .set(payload)
+            .returning('*')
+            .execute()
+            .then(res => res.raw[0]);
+    }
+
+    async delete(id: number) {
+        const exists = await this.existsById(id);
+
+        if (!exists) {
+            throw new AppError(404, 'Entity not found', this.namespace);
+        }
+
+        await this.db.manager
+            .createQueryBuilder()
+            .delete()
+            .from(this.targetEntity)
+            .where('id = :id', {id})
+            .execute();
+    }
+
+    protected async existsById(id: number) {
+        const select = this.db.manager
+            .createQueryBuilder()
+            .select('*')
+            .from(this.targetEntity, 'entity')
+            .where('entity.id = $1')
+            .getQuery();
+
+        const existsQuery = `select exists(${select})`;
+
+        const res = await this.db.manager.query(existsQuery, [id]);
+        return res?.[0]?.exists === true;
+    }
+
+    protected get namespace() {
+        return this.db.connection.getMetadata(this.targetEntity).tableName;
+    }
 }
