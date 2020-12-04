c54757b034096ccd7d792e17335b367c522091ee
Andrei Pampu <pampu.s.andrei@gmail.com>
2020-12-04 15:07:50 +0200

    Create koa logger factory

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,10 +1,26 @@
 import pino, {LoggerOptions} from 'pino';
+import pinoLogger from 'koa-pino-logger';
 
-export function createLogger(options: LoggerOptions) {
-    return pino({
-        prettyPrint: {colorize: true, translateTime: true},
+function createOptions(options?: LoggerOptions) {
+    return {
+        prettyPrint: {
+            colorize: true,
+            translateTime: true,
+        },
+        level: 'info',
         ...options,
-    });
+    } as LoggerOptions;
+}
+
+export function createLogger(options?: LoggerOptions) {
+    return pino(createOptions(options));
+}
+
+export function createKoaLogger(options?: LoggerOptions) {
+    return pinoLogger(createOptions({
+        level: 'warn',
+        ...options,
+    }));
 }
 
 export type Logger = pino.BaseLogger;
